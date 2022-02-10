0da2078d7f9b9bbf2fde457602a7673e29946257
ori davidi <oridavidi100@gmail.com>
2022-02-10 17:32:55 +0200

    mongo
    connect to mongo throw mongoose

--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -3,7 +3,17 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 const PORT = 5000;
 export const app = express();
-
+const { MONGO_URL } = require('./config');
+if (MONGO_URL) {
+  mongoose
+    .connect(MONGO_URL) // connect to mongodb
+    .then(() => {
+      console.log(`connected to MongoDB `);
+    })
+    .catch(error => {
+      console.log('error connecting to MongoDB:', error.message);
+    });
+}
 app.use(cors()); //cors middleware
 app.use(express.json()); //json middleware
 
