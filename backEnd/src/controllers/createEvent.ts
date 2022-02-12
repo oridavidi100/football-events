7bb5ac264acc41c02f143a50354c57193c0cbe98
ori davidi <oridavidi100@gmail.com>
2022-02-12 16:04:08 +0200

    create event

---
+++ backEnd/src/controllers/createEvent.ts
@@ -0,0 +1,23 @@
+import { Event } from '../models/Event';
+import { NextFunction, Request, Response } from 'express';
+import { User } from '../models/User';
+import { nanoid } from 'nanoid';
+exports.createEvent = (req: Request, res: Response, next: NextFunction) => {
+  console.log('hello');
+  const { location, date } = req.body;
+  if (!location || !date) {
+    throw { status: 400, message: 'missing fields' };
+  }
+  const user = req.body.user;
+  const players = [];
+  players.push(user.fullName);
+  const event = new Event({
+    _id: nanoid(),
+    location,
+    Players: players,
+    date,
+    creator: user.fullName,
+  });
+  event.save();
+  res.send(event);
+};
