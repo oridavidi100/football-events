02254e5a41559db71160caf5f839e431fa7207ce
ori davidi <oridavidi100@gmail.com>
2022-02-14 13:04:10 +0200

    building the router and starting with the login page

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from './NotFound';
+import Login from './Login';
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/:word/:partOfSpeech" element={<WordAndPos />} />
+          {/* <Route path="/:word/:partOfSpeech" element={<WordAndPos />} />
           <Route path="/:word" element={<Word />} />
-          <Route path="/part-of-speech/:part" element={<RandomWord />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/NotFound" element={<NotFound />} />
+          <Route path="/part-of-speech/:part" element={<RandomWord />} /> */}
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
