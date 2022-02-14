72e19a3584f671364b3b71abbd8f6ffac56d2f41
ori davidi <oridavidi100@gmail.com>
2022-02-14 16:35:09 +0200

    login design and funciunallty

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,7 +1,85 @@
-import React from 'react';
-
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './style/login.css';
 function Login() {
-  return <div>Login</div>;
+  const email = useRef<string | any>('');
+  const password = useRef<string | any>('');
+  const [error, setError] = useState<string | any>('');
+  const navigate = useNavigate();
+  useEffect(() => {
+    //function that check if the user is logged in before
+    const getCookie = (name: string) => {
+      const value = `; ${document.cookie}`;
+      const parts: any = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(';').shift();
+    };
+
+    if (getCookie('accessToken')) {
+      const config = {
+        headers: { Authorization: `Bearer ${getCookie('accessToken')}` },
+      };
+      axios
+        .post('http://localhost:5000/login', {}, config)
+        .then(res => {
+          console.log(res);
+          navigate('/HomePage');
+        })
+        .catch(err => {
+          console.log(err.response.data.error);
+        });
+    }
+  }, []);
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<any> => {
+    try {
+      e.preventDefault();
+      console.log(email.current.value);
+      console.log(password.current.value);
+      const res = await axios.post('http://localhost:5000/login', {
+        email: email.current.value,
+        password: password.current.value,
+      });
+      document.cookie =
+        'accessToken' + '=' + res.data.accessToken + ';expires=' + '120';
+      navigate('/HomePage');
+    } catch (err: any) {
+      console.log(err.response.data.error);
+      setError(err.response.data.error);
+    }
+  };
+  return (
+    <div className="loginDiv">
+      <div className="shape"></div>
+      <div className="shape"></div>
+      <form className="loginForm" onSubmit={e => handleSubmit(e)}>
+        <input type="text" placeholder="email " ref={email} required={true} />
+        <input
+          type="password"
+          placeholder="password"
+          ref={password}
+          required={true}
+        />
+        <button type="submit">Login</button>
+      </form>
+      <div className="errorMessage">{error}</div>
+      <button
+        className="loginOptionsButtons"
+        type="button"
+        onClick={() => navigate('/forgetpassword')}
+      >
+        forget password
+      </button>
+      <button
+        className="loginOptionsButtons"
+        type="button"
+        onClick={() => navigate('/register')}
+      >
+        register ?
+      </button>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './NotFound';
 import Login from './Login';
+import HomePage from './HomePage';
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* <Route path="/:word/:partOfSpeech" element={<WordAndPos />} />
-          <Route path="/:word" element={<Word />} />
-          <Route path="/part-of-speech/:part" element={<RandomWord />} /> */}
+          {/* <Route path="/:word/:partOfSpeech" element={<WordAndPos />} /> */}
+          {/* <Route path="/:word" element={<Word />} /> */}
+          <Route path="/HomePage" element={<HomePage />} />
           <Route path="/notfound" element={<NotFound />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </div>
     </Router>
