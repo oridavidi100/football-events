10640acb14365da0ffbdd9fc1d832ef8af399a4e
ori davidi <oridavidi100@gmail.com>
2022-02-15 13:07:52 +0200

    function set evnent
    the function of set events in the main page

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import './App.css';
+import axios from 'axios';
+import { setEvents } from '../reducer/actions/action';
+import { Event } from '../@types';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './NotFound';
 import Login from './Login';
 import HomePage from './HomePage';
+import Register from './Register';
+import ForgetPassword from './ForgetPassword';
+import { useSelector, useDispatch } from 'react-redux';
+import EventPage from './EventPage';
 function App() {
+  const dispatch = useDispatch();
+  const events = useSelector((state: any) => state.events);
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/getAllEvents')
+      .then(res => {
+        dispatch(setEvents(res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
   return (
     <Router>
       <div className="App">
         <Routes>
+          {events &&
+            events.map((event: Event) => {
+              return (
+                <Route path={event._id} element={<EventPage event={event} />} />
+              );
+            })}
+
           {/* <Route path="/:word/:partOfSpeech" element={<WordAndPos />} /> */}
-          {/* <Route path="/:word" element={<Word />} /> */}
+          <Route path="/forgetpassword" element={<ForgetPassword />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/HomePage" element={<HomePage />} />
           <Route path="/notfound" element={<NotFound />} />
           <Route path="/" element={<Login />} />
