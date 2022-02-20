ba8cffe57cd002d290b5ab8aebdbc4087668296d
ori davidi <oridavidi100@gmail.com>
2022-02-20 12:15:25 +0200

    chagne pos

--- client/src/components/ProfilePage.tsx
+++ client/src/components/ProfilePage.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
+import { changePos } from '../reducer/actions/action';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+const axios = require('axios');
+
 function ProfilePage() {
-  const user = useSelector((state: any) => state.user);
-  console.log(user, 'user');
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((state: any) => state.user);
+  const newPos = useRef<string | any>('');
+
+  useEffect(() => {
+    if (!document.cookie || !user) {
+      navigate('/');
+    }
+  });
+
+  const changePosition = async () => {
+    try {
+      const res = await axios.put(
+        'http://localhost:5000/getUser/changposition',
+        {
+          newPosition: newPos.current.value,
+          email: user.email,
+        }
+      );
+      dispatch(changePos(newPos.current.value));
+      newPos.current.value = '';
+      console.log(user);
+    } catch (err: any) {
+      console.log(err.response.data.error);
+    }
+  };
   return (
     <div>
       ProfilePage
@@ -13,6 +40,15 @@ function ProfilePage() {
           <p>{user.fullName}</p>
           <p>{user.email}</p>
           <p>{user.position}</p>
+          <input
+            ref={newPos}
+            type="text"
+            required={true}
+            placeholder="new position "
+          />
+          <button type="button" onClick={changePosition}>
+            change position
+          </button>
         </div>
       )}
     </div>
