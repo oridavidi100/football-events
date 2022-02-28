ffc45fb48b1a09b5d96af8201977b1aef2bbcae2
ori davidi <oridavidi100@gmail.com>
2022-02-28 15:46:15 +0200

    testing
    testing using jest and super test

--- backEnd/src/config.ts
+++ backEnd/src/config.ts
@@ -1,4 +1,8 @@
 require('dotenv').config();
-const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL = process.env.MONGO_URL;
 const secret = process.env.SECRET_KEY;
+const MONGO_URL =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGO_URL;
 export default { secret, MONGO_URL };
