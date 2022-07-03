ef4bf22f515af3e91beee2d9fed4e298ce1043b9
ori davidi <oridavidi100@gmail.com>
2022-07-03 18:36:24 +0300

    adding city interface

--- client/src/@types.ts
+++ client/src/@types.ts
@@ -37,3 +37,14 @@ export interface ClientToServerEvents {
     room: string;
   }) => void;
 }
+export interface City {
+  english_name: string;
+  lishka: string;
+  name: string;
+  semel_lishkat_mana: string;
+  semel_moatza_ezorit: string;
+  semel_napa: string;
+  semel_yeshuv: string;
+  shem_moaatza: string;
+  shem_napa: string;
+}
